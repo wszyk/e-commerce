84f264bc6948dca00e219052b275577bba2a78a9
巭孬嫑夯 <www.1342944009@qq.com>
2019-01-08 17:03:12 +0800

    add redis cache

--- shopping/project-admin-api/src/main/java/com/zyk/projectadminapi/ProjectAdminApiApplication.java
+++ shopping/project-admin-api/src/main/java/com/zyk/projectadminapi/ProjectAdminApiApplication.java
@@ -2,9 +2,10 @@ package com.zyk.projectadminapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages="com.zyk")
+@EnableCaching
 public class ProjectAdminApiApplication {
 
     public static void main(String[] args) {
--- shopping/project-admin-api/src/main/java/com/zyk/projectadminapi/admin/controller/UserController.java
+++ shopping/project-admin-api/src/main/java/com/zyk/projectadminapi/admin/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.zyk.projectadminapi.admin.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageInfo;
+import com.zyk.projectadminapi.admin.dto.Authcode;
 import com.zyk.projectadminapi.admin.dto.LoginInfo;
 import com.zyk.projectadminapi.admin.exception.BackendClientException;
+import com.zyk.projectadminapi.admin.mail.MailService;
 import com.zyk.projectservice.dto.AddUser;
 import com.zyk.projectservice.dto.UserListDTO;
 import com.zyk.projectservice.dto.UserUpdateDTO;
@@ -12,6 +15,8 @@ import com.zyk.projectservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +31,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private com.zyk.projectadminapi.admin.mail.MailService MailService;
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @GetMapping("/getById")
     public User getById(@RequestParam Long userId){
@@ -58,29 +67,39 @@ public class UserController {
         String token = Base64.getEncoder().encodeToString(loginInfoStr.getBytes());
         return token;
     }
-    @GetMapping("/resetPassword")
-    @Cacheable("admin")
-    public String resetPassword(String email){
 
-        int i=(int)((Math.random()*9+1)*100000);
-        String authcode = String.valueOf(i);
+
+    @GetMapping("/resetPassword")
+    public void resetPassword(String email){
+
+        int authcode=(int)((Math.random()*9+1)*100000);
+        String str = String.valueOf(authcode);
+        MailService.sendSimpleMail("1342944009@qq.com","test simple mail",str);
         //1. 随机生成随机码
         //2. 发送随机到email
         //3. email as key ,random code as value, store to redis (expire time)
-        return email+authcode;
+      redisTemplate.opsForValue().set(email,str);
     }
 
     @GetMapping("/verifyCode")
-    public void verifyEmailCode(HttpServletRequest request,@RequestParam String email, @RequestParam String code){
+    public void verifyEmailCode(HttpServletRequest request,@RequestParam String email, @RequestParam String code,@RequestParam String password) throws BackendClientException {
 
-        String str=email+code;
+        String rediscode = (String) redisTemplate.opsForValue().get(email);
+         if (rediscode==null){
+             throw new BackendClientException("rediscode is null");
+         }
+         if(!code.equals(rediscode)){
+             throw new BackendClientException("email verify code is invalid");
+         }
+//        changePassword(email,password);
         //1. get code by email from redis
         //   code null 过期 抛异常
         //2. compare input code with redis code
         //3. 相同验证通过，密码重置123456，
         //   否则验证不同 throw exception
-
+        System.out.println(email+password);
         String authorizationStr = request.getHeader("Authorization");
+        System.out.println(authorizationStr);
         //自定修改，返回Token，授权一个修改密码的api
 
 
